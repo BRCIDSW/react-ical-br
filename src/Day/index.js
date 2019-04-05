3279112df266573bf2b3d260c6be9b69bf2add9f
oflatley <oflatley@gmail.com>
2019-04-05 06:55:07 -0700

    asdfa

--- src/Day/index.js
+++ src/Day/index.js
@@ -90,12 +90,12 @@ export default class Day extends PureComponent {
         style={color ? {color} : null}
         className={classNames(styles.root, {
           
-          // [styles.today]: (isToday && !inSelectionRange),
+          [styles.today]: (isToday && !inSelectionRange),
           // [styles.highlighted]: isHighlighted,
 
           // // [styles.selected]: (isSelected && !inSelectionRange),
           
-          // [styles.disabled]: isDisabled,
+          [styles.disabled]: isDisabled,
           // [styles.enabled]: !isDisabled,
         
           [styles.rangeOpen]: isSelectionStart,
