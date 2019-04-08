abfe016a1a24bf34d0a6cdb9db8078bdec8a7032
oflatley <oflatley@gmail.com>
2019-04-08 06:52:27 -0700

    asdf

--- src/Day/index.js
+++ src/Day/index.js
@@ -83,6 +83,12 @@ export default class Day extends PureComponent {
     // mark each sunday of a dirty week 
     var markDirty = isInDirtyWeek && dayOfWeek === 6;
 
+    if( isSelectionEnd )
+    {
+      console.log( "today: " + isToday + "  high: " + isHighlighted + "  selected: " + (isSelected && !inSelectionRange) + "  inSelectionRange: " + (isSelected && inSelectionRange) + "  disable: " + isDisabled + "  enabled: " + (!(isDisabled || inSelectionRange)) ); 
+    }
+
+
     return (
       <li
         style={color ? {color} : null}
